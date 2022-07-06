c143283f08cfbdf6f34538e8b5828484193e6468
Codeyg12 <codey.gallup@gmail.com>
2022-07-05 18:41:38 -0600

    added close button fucntion

--- scribe.js
+++ scribe.js
@@ -1,48 +1,52 @@
+$(document).ready(function() {
 
-<<<<<<< HEAD
-=======
-    $("#launchHome").click(function() {
- 
-         $("#hero").addClass("is-hidden"); 
+  $("#launchHome").click(function() {
 
-         $("#main-content").removeClass("is-hidden"); 
-          
-         function generateQuote(){
-            fetch('https://api.quotable.io/random')
-           .then(function(data) {
-                  return data.json();
-             })
-             .then(function(data){    
-             document.getElementById("quote").innerHTML = '"' + data.content + '"'; 
-             document.querySelector(".author").innerHTML = "- " + data.author;
-            })
-          .catch(function(err) {
-             console.log(err); 
-             });
-          }
-        });
+       $("#hero").addClass("is-hidden"); 
 
-    // let currentDay = moment().format("dddd, MMMM Do YYYY")
-    // $("#currentDay").text(currentDay)
+       $("#main-content").removeClass("is-hidden"); 
+        
+          fetch('https://api.quotable.io/random')
+         .then(function(data) {
+                return data.json();
+           })
+           .then(function(data){    
+           document.getElementById("quote").innerHTML = '"' + data.content + '"'; 
+           document.querySelector(".author").innerHTML = "- " + data.author;
+          })
+        .catch(function(err) {
+           console.log(err); 
+           });
+        
+      });
 
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-  
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-  
+  // let currentDay = moment().format("dddd, MMMM Do YYYY")
+  // $("#currentDay").text(currentDay)
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  $("#launchModal").click(function() {
+
+      $(".modal").addClass("is-active"); 
+     
     });
 
-    $("#launchModal").click(function() {
- 
-        $(".modal").addClass("is-active"); 
-       
-      });
+  $(".modal-close").click(function() {
 
-    $(".modal-close").click(function() {
- 
-        $(".modal").removeClass("is-active"); 
-       
-      });
-  });
->>>>>>> 1065fda1ac5f3236c42bc59a8f93256a1415696d
+      $(".modal").removeClass("is-active"); 
+     
+    });
+});
+
+let cancelBtn = $("#cancel") 
+cancelBtn.click(function () {
+  $(".modal").removeClass("is-active")
+})
+
+
