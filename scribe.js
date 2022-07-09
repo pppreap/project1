f0a4a4e9549f21b4fdc59c5318abed105704aadd
Codeyg12 <codey.gallup@gmail.com>
2022-07-09 09:01:46 -0600

    location access

--- scribe.js
+++ scribe.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
     if (response.ok) {
       // Update DOM elements
       quote.textContent = '"'+ data.content + '"';
+      quote.classList.add("glow", "is-size-5")
       author.textContent = '-'+ data.author;
+      author.classList.add("glow", "is-size-6")
     } else {
       quote.textContent = "An error occured";
       console.log(data);
@@ -20,25 +22,75 @@ $(document).ready(function() {
   updateQuote();
 });
 
+
+
+$(document).ready(function() {
+
+  $("#launchHome").click(function() {
+
+       $("#hero").addClass("is-hidden"); 
+
+       $("#main-content").removeClass("is-hidden"); 
+        
+      
+          fetch('https://api.quotable.io/random')
+         .then(function(data) {
+                return data.json();
+           })
+           .then(function(data){    
+           document.getElementById("quote").innerHTML = '"' + data.content + '"'; 
+           document.querySelector(".author").innerHTML = "- " + data.author;
+          })
+        .catch(function(err) {
+           console.log(err); 
+           });
+        
+      });
+
+  // let currentDay = moment().format("dddd, MMMM Do YYYY")
+  // $("#currentDay").text(currentDay)
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  $("#launchModal").click(function() {
+
+      $(".modal").addClass("is-active"); 
+     
+    });
+
+  $(".modal-close").click(function() {
+
+      $(".modal").removeClass("is-active"); 
+     
+    });
+});
+
 let cancelBtn = $("#cancel") 
 cancelBtn.click(function () {
   $(".modal").removeClass("is-active")
 })
 
 
-$.getJSON("https://api.ipify.org/?format=json", function(e) {
-    console.log(e.ip);
-    console.log(e)
+
+let locationShare = $("#yesShare")
+if (locationShare) {
+  $.getJSON("https://api.ipify.org/?format=json", function(e) {
     let ipKey = e.ip
     $.ajax({
       url: "http://api.positionstack.com/v1/reverse?access_key=fede56057a8e01c98e320fd27297bdfc&query=" + ipKey,
       method: "GET"
     }).then(function (data) {
-      console.log(data)
-      console.log(data.data[0].locality)
-      console.log(data.data[0].country)
       console.log(data.data[0].country_code)
-      console.log(data.data[0].region)
       console.log(data.data[0].region_code)
+console.log(data)
+console.log(data.data[0].locality)
     })
-});
+  });
+  
+}
